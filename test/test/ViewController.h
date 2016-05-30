2896d48ddda6f9261bbf6c8caeb8e738231f1ea4
szastan <szastan@gmail.com>
2016-05-30 19:50:58 +0200

    Update ViewController.h

--- test/test/ViewController.h
+++ test/test/ViewController.h
@@ -5,7 +5,7 @@
 //  Created by Kamil Schild on 30.05.2016.
 //  Copyright © 2016 Kamil Schild. All rights reserved.
 //
-
+// to jest linia dopisana ręcznie
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
