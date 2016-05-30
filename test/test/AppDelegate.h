ae7f2c296aef72159098b00116abe1e9fe73c7ab
szastan <szastan@gmail.com>
2016-05-30 19:46:04 +0200

    pierwsza wersja

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Kamil Schild on 30.05.2016.
+//  Copyright © 2016 Kamil Schild. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Kamil Schild on 30.05.2016.
+//  Copyright © 2016 Kamil Schild. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
